731cdd95f76f0b2e9b9d96c5ee48ec17793ab7e7
tabtsai <tabtsai@gmail.com>
2015-09-30 20:11:42 -0500

    forum js modal added,

---
+++ js/ask_modal.js
@@ -0,0 +1,23 @@
+$newQuestion = $('<li></li>');
+$newTag = $('<p class="tag"></p>');
+
+$( "#ask_modal" ).click(function() {
+   $('.modal').show( "slow" );
+});
+
+$( ".close" ).click(function() {
+   $('.modal').hide( "slow" );
+});
+
+
+$('.buttonsubmit').click(function(){
+	var value = $("textarea").val();
+	$newQuestion.text(value).prependTo('.questions');
+	
+	var tag = $( "#tag option:selected").val();
+	$newTag.text(tag).appendTo($newQuestion);
+
+	$('.modal').hide( "slow" );
+
+});
+ 
