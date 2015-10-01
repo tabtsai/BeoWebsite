d0f0feaae474dad2cb0f2dbe6882f6044a073a0a
tabtsai <tabtsai@gmail.com>
2015-10-01 17:51:05 -0500

    course preview modal added for honeycomb
    video demo added to new video folder

---
+++ js/course_modal.js
@@ -0,0 +1,67 @@
+
+/************
+Modal Show/Hide
+*************/
+
+$( "#course_modal" ).click(function() {
+  $('.wrap').show( "slow" );
+});
+
+$( ".close" ).click(function() {
+  $('.wrap').hide( "slow" );
+});
+
+/************
+Slideshow
+*************/
+	
+	var currIndex = 0;
+	var totalSlides = $(".slideshow li").length;
+
+	
+  function next(){
+		var nextIndex = currIndex + 1;
+		if (nextIndex >= totalSlides){
+			nextIndex = 0;
+		}
+    gotoSlide(nextIndex);
+
+  }
+
+  function prev(){
+	  
+		var prevIndex = currIndex - 1;
+		
+		if (prevIndex < 0){
+			prevIndex = totalSlides - 1;
+		}
+    gotoSlide(prevIndex);
+  }
+
+	function gotoSlide( $index ){
+		currIndex = $index;
+		var target = "#slide"+currIndex;
+		$(".slideshow li, nav ul li").removeClass("active");		
+		
+		$(target).addClass("active");
+		$("nav ul li").eq(currIndex).addClass("active");
+
+	}
+	
+	$("#next").on("click", function(){
+		next();
+	});
+
+	$("#prev").on("click", function(){
+		next();
+	});
+	
+
+	
+/************
+Video
+*************/
+
+$("video").bind("ended", function() {
+    $('#end-poster').show();
+});
