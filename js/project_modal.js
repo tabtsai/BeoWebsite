6fe00b033ff5601005d421593b8e2694358eea71
tabtsai <tabtsai@gmail.com>
2015-10-01 21:13:00 -0500

    project modal injection
    CANDIDATE FOR GOING UNDOING THIS COMMIT

---
+++ js/project_modal.js
@@ -0,0 +1,58 @@
+
+/************
+Modal Show/Hide
+*************/
+
+$( "#project_modal" ).click(function() {
+  $('.overlay').show( "slow" );
+});
+
+$( ".close" ).click(function() {
+  $('.overlay').hide( "slow" );
+});
+
+/************
+Slideshow
+*************/
+	
+	var currIndex = 0;
+	var totalSlides = $(".slideshow > li").length;
+
+	
+  function next(){
+		var nextIndex = currIndex + 1;
+		if (nextIndex >= totalSlides){
+			nextIndex = 0;
+		}
+    gotoSlide(nextIndex);
+
+  }
+
+  function prev(){
+	  
+		var prevIndex = currIndex - 1;
+		
+		if (prevIndex < 0){
+			prevIndex = totalSlides - 1;
+		}
+    gotoSlide(prevIndex);
+  }
+
+	function gotoSlide( $index ){
+		currIndex = $index;
+		var target = "#slide"+currIndex;
+		$(".slideshow>li, nav ul li").removeClass("active");		
+		
+		$(target).addClass("active");
+		$("nav ul li").eq(currIndex).addClass("active");
+
+	}
+	
+	$("#next").on("click", function(){
+		next();
+	});
+
+	$("#prev").on("click", function(){
+		next();
+	});
+	
